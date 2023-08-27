5bd3a3719a2edd11c0aff7629a6b22061a8bf8f9
Morioka-Ryosuke06 <mrok.ryosuke@gmail.com>
2023-08-28 00:20:25 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  svelteOptions: {
-    kit: {
-      // 他の設定...
-      vite: {
-        // グローバルな JavaScript ファイルを指定
-        optimizeDeps: {
-          include: ['./src/modules/main.js'],
-        },
-      },
-    },
-  },
-});
+export default defineConfig({});
